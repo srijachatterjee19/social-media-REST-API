fc9cbcbd70ee16b2a7b987b84c72019409b85e67
srijachatterjee <sc1698@student.aru.ac.uk>
2024-02-27 17:06:14 +0000

    Login route added

--- routes/auth.js
+++ routes/auth.js
@@ -20,9 +20,27 @@ router.post("/register", async (req,res) =>{
         // save user and return response
         const user = await newUser.save();
         res.status(200).json(user);
+
     } catch (error) {
-        console.log(error);
+        res.status(500).json(error);
     }
 });
+
+// LOGIN
+router.post("/login", async(req,res) =>{
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        // check if username is valid
+        !user && res.status(404).send("user not found");
+         // check if password is valid
+         const validPassword = await bcrypt.compare(req.body.password, user.password);
+         !validPassword && res.send(400).json("wrong password");
+         // if username and password are both valid
+         res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
 
 module.exports = router;
