1cc2ffb5dffe1b254f5cfd4a5dd64b2b9ad3982d
srijachatterjee <sc1698@student.aru.ac.uk>
2024-02-27 14:16:00 +0000

    Added register router and user schema

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 const app = express();
 
@@ -28,6 +29,7 @@ app.use(helmet());
 app.use(morgan("common"));
 
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 
 app.listen(8800,()=>{
