12d5e2f8b4af4883d80e698bcb8c74f73abe698d
srijachatterjee <sc1698@student.aru.ac.uk>
2024-02-27 18:17:58 +0000

    Update post route

--- routes/posts.js
+++ routes/posts.js
@@ -12,6 +12,20 @@ router.post("/", async (req,res) =>{
     }
 });
 //update post
+router.put("/", async (req,res)=>{
+    try {
+        const post = await Post.findById(req.params.id);
+        //if post belongs to current user 
+        if(post.userId === req.body.userId){
+            await post.updateOne({$set: req.body}); //update post schema
+            res.status(200).json("post has been updated");
+        }else{
+            res.status(403).json("you can only update your post");
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 //delete post
 //like post
 //get post
