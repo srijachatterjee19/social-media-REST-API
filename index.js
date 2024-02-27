20c0836f860507e63c0718f1887f53c380e28fe4
srijachatterjee <sc1698@student.aru.ac.uk>
2024-02-27 13:47:16 +0000

    Routes initialised

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const morgan = require('morgan');
-
+const userRoute = require("./routes/users");
 
 const app = express();
 
@@ -27,13 +27,8 @@ app.use(express.json()); //parse request body after making requests
 app.use(helmet());
 app.use(morgan("common"));
 
-app.get("/", (req,res)=>{
-    res.send("Welcome to homepage");
-});
+app.use("/api/users", userRoute);
 
-app.get("/users", (req,res)=>{
-    res.send("Welcome to user page");
-});
 
 app.listen(8800,()=>{
     console.log("Backend server is running");
