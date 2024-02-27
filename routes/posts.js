0492299af327a1c8d4d343f82e2b6465484642ad
srijachatterjee <sc1698@student.aru.ac.uk>
2024-02-27 18:27:14 +0000

    Get a post route

--- routes/posts.js
+++ routes/posts.js
@@ -59,6 +59,14 @@ router .put("/:id/like",async (req,res) =>{
     }
 });
 //get post
+router.get("/:id", async (req,res) =>{
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
 //get timeline posts
 
 module.exports = router;
