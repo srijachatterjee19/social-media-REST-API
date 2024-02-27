d92cea77f337753106a1dec1dabed1cb1be8f3f9
srijachatterjee <sc1698@student.aru.ac.uk>
2024-02-27 13:40:29 +0000

    Refactored mongodb connection code block

--- index.js
+++ index.js
@@ -8,14 +8,21 @@ const morgan = require('morgan');
 const app = express();
 
 dotenv.config();
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+// Connect to MongoDB
+async function connectToMongoDB() {
+    try {
+      await mongoose.connect(process.env.MONGO_URL);
+      console.log("Connected to MongoDB");
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+      // Handle the error appropriately, e.g., exit the application
+      process.exit(1);
+    }
+  }
+  
+  connectToMongoDB();
 
+  
 app.use(express.json()); //parse request body after making requests
 app.use(helmet());
 app.use(morgan("common"));
