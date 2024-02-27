a49a4455d683bccbcf03ba798bd750667e14b6fe
srijachatterjee <sc1698@student.aru.ac.uk>
2024-02-27 13:34:00 +0000

    Created basic middlewares

--- index.js
+++ index.js
@@ -16,6 +16,18 @@ mongoose.connect(process.env.MONGO_URL)
     console.error("Error connecting to MongoDB:", error);
   });
 
+app.use(express.json()); //parse request body after making requests
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req,res)=>{
+    res.send("Welcome to homepage");
+});
+
+app.get("/users", (req,res)=>{
+    res.send("Welcome to user page");
+});
+
 app.listen(8800,()=>{
     console.log("Backend server is running");
-}) 
+});
