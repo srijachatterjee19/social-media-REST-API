c71b33b2feb075d6d81224db00df20d06ca7bcd7
srijachatterjee <sc1698@student.aru.ac.uk>
2024-02-27 13:30:06 +0000

    Added mongodb connection

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ const morgan = require('morgan');
 const app = express();
 
 dotenv.config();
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 app.listen(8800,()=>{
     console.log("Backend server is running");
-})
+}) 
