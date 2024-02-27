aa8e121cd0e614bdd44033f81a32efd9b1b75091
srijachatterjee <sc1698@student.aru.ac.uk>
2024-02-27 18:09:38 +0000

    Added post schema and create post route

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 const app = express();
 
@@ -30,6 +31,7 @@ app.use(morgan("common"));
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 
 app.listen(8800,()=>{
---
+++ routes/posts.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const Post = require("../models/Post");
+
+//create post
+router.post("/", async (req,res) =>{
+    const newPost =  new Post(req.body);
+    try {
+        const savedPost = await newPost.save();
+        res.status(200).json(savedPost);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+//update post
+//delete post
+//like post
+//get post
+//get timeline posts
+
+module.exports = router;
